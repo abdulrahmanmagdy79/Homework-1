bec78f21f8a93228df263c87e85372b6d6345d2d
abdulrahmanmagdy79 <20201700480@cis.asu.edu>
2025-08-16 20:09:43 +0300

    first commit

---
+++ HW1 of Session 2/lib/Exp.2.dart
@@ -0,0 +1,18 @@
+/*
+2. Describe the difference between var, dynamic, and explicitly typed variables in Dart
+
+Answer:
+
+The difference between var, dynamic, and explicitly typed variables in Dart is as follows:
+
+var: allows you to declare different-type variables. It's considered as a KEYWORD not a data type, 
+as well as it's type is known from the value assigned to it at runtime.
+
+dynamic: is used to declare a variable whose type is not known at compile-time. Dynamic can be used 
+when you don't know the type of the variable. It's cosidered as a weak data type and like var, it's type is known from the value assigned to it.
+
+Explicitly typed variables: like int, double, String
+are used to declare a variable whose type is known at compile-time. 
+It's considered as a strong data type and it's type is known from direct declaration. 
+
+ */
